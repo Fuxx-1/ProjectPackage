83b3139f92c46eeeba31fe42d0a362e2f2976d14
Fuxx-1 <583742849@qq.com>
2022-06-06 21:00:53 +0800

    update UserTest

--- UserTest/data/MySQLData/sql/init.sql
+++ UserTest/data/MySQLData/sql/init.sql
@@ -2,4 +2,4 @@
 CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
